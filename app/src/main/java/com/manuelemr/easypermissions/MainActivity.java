d00aed2636dcab9f81157f473da58f12cb1e0ba5
Manuel Munoz <manuelmunozr.92@gmail.com>
2017-01-10 21:17:35 -0400

    :scissors: Changed MainActivity sample code

--- app/src/main/java/com/manuelemr/easypermissions/MainActivity.java
+++ app/src/main/java/com/manuelemr/easypermissions/MainActivity.java
@@ -34,32 +34,32 @@ public class MainActivity extends AppCompatActivity {
                                 for(int i=0; i<permissions.length; i++){
                                     Permission permission = permissions[i];
 
-                                    switch (permission.getName()){
+                                    if (permission.getName().equals(Manifest.permission.CAMERA)) {
 
-                                        case Manifest.permission.CAMERA:
-                                            if(permission.isGranted()){
-                                                //User granted permission continue and use camera
-                                            }
-                                            else if(permission.shouldShowRequestRationale()){
-                                                //User has denied permission once and have not selected 'dont ask again'
-                                            }
-                                            else {
-                                                //User has denied permission with 'Dont ask again' selected
-                                            }
-                                            break;
-                                        case Manifest.permission.READ_CONTACTS:
-                                            if(permission.isGranted()){
-                                                //User granted permission continue and use phone contacts
-                                            }
-                                            else if(permission.shouldShowRequestRationale()){
-                                                //User has denied permission once and have not selected 'dont ask again'
-                                            }
-                                            else {
-                                                //User has denied permission with 'Dont ask again' selected
-                                            }
-                                            break;
+                                        if (permission.isGranted()) {
+                                            //User granted permission continue and use camera
+                                            Log.d("Permission", Manifest.permission.CAMERA + " Granted");
+                                        } else if (permission.shouldShowRequestRationale()) {
+                                            //User has denied permission once and have not selected 'dont ask again'
+                                            Log.d("Permission", Manifest.permission.CAMERA + " Show Rationale");
+                                        } else {
+                                            //User has denied permission with 'Dont ask again' selected
+                                            Log.d("Permission", Manifest.permission.CAMERA + " Dont ask again");
+                                        }
                                     }
+                                    else if(permission.getName().equals(Manifest.permission.READ_CONTACTS)) {
 
+                                        if (permission.isGranted()) {
+                                            //User granted permission continue and use phone contacts
+                                            Log.d("Permission", Manifest.permission.READ_CONTACTS + " Granted");
+                                        } else if (permission.shouldShowRequestRationale()) {
+                                            //User has denied permission once and have not selected 'dont ask again'
+                                            Log.d("Permission", Manifest.permission.READ_CONTACTS + " Show Rationale");
+                                        } else {
+                                            //User has denied permission with 'Dont ask again' selected
+                                            Log.d("Permission", Manifest.permission.READ_CONTACTS + " Dont ask again");
+                                        }
+                                    }
                                 }
                             }
                         });
